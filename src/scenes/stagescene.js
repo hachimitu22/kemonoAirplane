c3d8e06511e8ed5e6d90beb9acb1b4f0dd7d11af
hachimitu22 <hachimitu22.888@gmail.com>
2019-11-21 06:11:55 +0900

    change: スコア加算まで出来るようにした

--- src/scenes/stagescene.js
+++ src/scenes/stagescene.js
@@ -24,7 +24,33 @@ phina.namespace(function () {
       this.kaban = Kaban().addChildTo(this).setPosition(100, 200);
       this.enemys = phina.display.DisplayElement().addChildTo(this);
       this.attackers = phina.display.DisplayElement().addChildTo(this);
-      this.t = 0;
+      this.uiGroup = phina.display.DisplayElement().addChildTo(this);
+      this.shotPointerPanel = phina.display.RectangleShape({
+        width: 500,
+        height: 300,
+        stroke: 0,
+        fill: '#8080FF80',
+        x: 400,
+        y: 150,
+      }).addChildTo(this).setInteractive(true).on('pointstart', function (e) {
+        var x = e.pointer.x;
+        var y = e.pointer.y;
+
+        self.shotPointer.setPosition(x, y);
+        self._shot();
+      }).on('pointmove', function (e) {
+        var x = Math.clamp(e.pointer.x, this.left, this.right);
+        var y = Math.clamp(e.pointer.y, this.top, this.bottom);
+
+        self.shotPointer.setPosition(x, y);
+      });
+      this.shotPointer = phina.display.CircleShape({
+        radius: 5,
+        fill: "#FF000080",
+        x: 200,
+        y: 100,
+      }).addChildTo(this).setPosition(this.shotPointerPanel.centerX, this.shotPointerPanel.centerY);
+      this.t = 1;
 
       this.spawner.setSpawnParam({
         'x': kfap.WIDTH_SIZE + 50,
@@ -34,37 +60,63 @@ phina.namespace(function () {
       this.spawner.setInterval(100);
 
       var self = this;
+      this.score = 0;
+      this.scoreLabel = phina.display.Label({
+        fill: '#FFFFFF',
+        stroke: 'black',
+        fontSize: 12,
+        strokeWidth: 2,
+      }).addChildTo(this.uiGroup).setOrigin(0, 0).setPosition(10, 10);
+      this._updateScore(0);
+
       var pauseButton = phina.ui.Button({
         text: 'ポーズ',
         fontSize: 16,
         width: 100,
         height: 40,
-        x: this.gridX.center(),
+        x: 50,
         y: this.gridY.center(),
-      }).addChildTo(this);
-
-      pauseButton.on('push', function () {
+      }).addChildTo(this.uiGroup).on('push', function () {
         self.app.pushScene(PauseScene());
       });
 
+    },
+
+    _updateScore: function (diffScore) {
+      this.score += diffScore;
+      this.scoreLabel.text = (this.score).padding(7, ' ');
+    },
+
+    _shot: function () {
+      var shot = Shot(90, 255);
+      var vx = (this.shotPointer.x - this.kaban.x) * 0.05;
+      var vy = (this.shotPointer.y - this.kaban.y) * 0.05;
+      shot.addChildTo(this.attackers);
+      // shot.takeoff(Math.randfloat(0, 10), Math.randfloat(-20, 0), 0.99, 0.2);
+      shot.takeoff(vx, vy, 0.99, 0.2);
     },
 
     update: function (app) {
 
       {
         this.t++;
-        if (this.t % 3 === 0) {
+        if (this.t % (app.fps * 2) === 0) {
+          // this._shot();
           // var shot = Shot(90, 255);
+          // var vx = (this.shotPointer.x - this.kaban.x) * 0.05;
+          // var vy = (this.shotPointer.y - this.kaban.y) * 0.05;
           // shot.addChildTo(this.attackers);
-          // shot.takeoff(Math.randfloat(0, 10), Math.randfloat(-20, 0), 0.99, 0.2);
+          // // shot.takeoff(Math.randfloat(0, 10), Math.randfloat(-20, 0), 0.99, 0.2);
+          // shot.takeoff(vx, vy, 0.99, 0.2);
+          this.t = 0;
         }
       }
 
-      if (app.keyboard.getKeyDown('A')) {
-        var shot = Shot(90, 255);
+      // if (app.keyboard.getKeyDown('A')) {
+      //   var shot = Shot(90, 255);
 
-        shot.addChildTo(this.attackers);
-      }
+      //   shot.addChildTo(this.attackers);
+      // }
 
       // collision
       {
@@ -76,6 +128,8 @@ phina.namespace(function () {
             self.enemys.children.eraseIf(function (enemy) {
               if (enemy.isHitEnable() && Collision.testCircleRect(enemy, attacker)) {
                 flag = true;
+                this.score += 1;
+                self._updateScore(1);
                 return true;
               }
             });
